c439d80fabc890386e04ec0ef181daf7de37d239
Giovanni <anderlansouza71@gmail.com>
2024-12-15 16:25:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function clear(text, Input){
+    text = "";
+    Input.value = text
+}
+
+function chameOnClick(operado) {
+    let textInput = document.getElementsByName("input-cal")[0];
+    let text =  textInput.value
+    if (operado != "C" && operado != "="){
+        if (text.length === 0 || !isNaN(text[text.length - 1])) {
+            if (text.length === 0 && operado == "0"){
+                clear()
+            } else {
+                text += operado;
+                textInput.value = text
+            }
+        } else if (isNaN(text[text.length - 1]) && !isNaN(operado) && text.length !== 0) {
+            text += operado;
+            textInput.value = text
+        } 
+    }  else if (operado === "="){
+        try { 
+            textInput.value = eval(text);
+        } catch (e) {
+            alert("Erro ao calcular valor... Digite um valor válido.");
+            clear(text, textInput)
+        }
+    } else {
+        clear(text, textInput)
+    } 
+}
+
+
+
+
